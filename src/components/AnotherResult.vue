8b3f140e05e6b8db9cb45a8d91046d4a228ca911
Wan Cheul Kim <wckim88@gmail.com>
2019-02-28 20:34:12 -0800

    counter methods updated to use getters instead of state

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Double Counter: {{ counter }}</p>
+        <p>Click Counter: {{ clicks }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            counter() {
+                return this.$store.getters.doubleCounter;
+            },
+            clicks() {
+                return this.$store.getters.stringCounter;
+            }
+        }
+    }
+</script>
